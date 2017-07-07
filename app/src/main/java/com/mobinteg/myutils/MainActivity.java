221396562ee319bcc532c2dff9ffac31b207a2ae
P M <pmartins@mobinteg.com>
2017-07-07 18:20:10 +0100

    updated

--- app/src/main/java/com/mobinteg/myutils/MainActivity.java
+++ app/src/main/java/com/mobinteg/myutils/MainActivity.java
@@ -1,23 +1,84 @@
 package com.mobinteg.myutils;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.mobinteg.utilslib.MyUtils;
+import com.mobinteg.utilslib.util.ConvertUtils;
+import com.mobinteg.utilslib.util.Utils;
 
-import java.util.Locale;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Context mContext;
+    private WebView webview;
+    private String content;
+    private String url = "http://autarquicas.psd.pt/index.php/site/candidato?id=199";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv = (TextView) findViewById(R.id.tv);
+        Utils.init(this);
 
-        tv.setText(MyUtils.convertDate("Qui Nov 03 2016", "EEE MMM dd yyyy", "EEE dd-MM-yyyy", new Locale("pt", "PT"), Locale.US, true));
+        ConvertUtils.dp2px(100);
+
+        mContext = this;
+
+        webview = (WebView)findViewById(R.id.webview);
+/*
+        Async async = new Async();
+        async.execute();
+*/
+
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url)
+            {
+                webview.loadUrl("javascript:(function() {document.getElementsByClassName(\"header\")[0].style.display='none';})()");
+                webview.loadUrl("javascript:(function() {document.getElementsByClassName(\"footer-above\")[0].style.display='none';})()");
+                webview.loadUrl("javascript:(function() {document.getElementsByClassName(\"footer\")[0].style.display='none';})()");
+            }
+        });
+        webview.loadUrl(url);
 
     }
+
+    class Async extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            webview.getSettings().setJavaScriptEnabled(true);
+            webview.loadDataWithBaseURL("", url, "text/html", "UTF-8", "");
+            //webview.loadUrl(content);
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            Document document = null;
+            try {
+                document = Jsoup.connect(url).get();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            content = document.select("div.candidato").outerHtml();
+
+            return null;
+        }
+    }
+
 }
--- utilslib/src/main/java/com/mobinteg/utilslib/MyUtils.java
+++ utilslib/src/main/java/com/mobinteg/utilslib/MyUtils.java
@@ -4,13 +4,23 @@ package com.mobinteg.utilslib;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Point;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.text.format.DateFormat;
 import android.util.DisplayMetrics;
 import android.view.Display;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Random;
+import java.util.UUID;
 
 public class MyUtils {
 
@@ -85,5 +95,86 @@ public class MyUtils {
         return desiredFormat.format(date.getTime());
     }
 
+    /**
+     *
+     * @param dims
+     * @return Returns an image with size 600*600 if no dimensions parameters are specified
+     */
 
+    public static String image(int... dims){
+
+        int width = 600;
+        int height = 600;
+
+        Random r = new Random();
+        int n = r.nextInt(1000);
+
+        if(dims!=null && dims.length==2){
+            width = dims[0];
+            height = dims[1];
+        }
+        return "https://unsplash.it/" + width + "/" + height + "?image=" + n;
+    }
+
+
+    public static void ud(Context mContext, String... str) {
+        String text = "Feature under development";
+        if(str!=null){
+            text= str[0];
+        }
+        Toast.makeText(mContext, text, Toast.LENGTH_SHORT).show();
+    }
+
+    public static String currentDate(String s) {
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat(s, Locale.US);
+        return df.format(c.getTime());
+    }
+
+    public static String randomId() {
+        return UUID.randomUUID().toString();
+    }
+
+    public static void hideKeyboard(Activity activity) {
+        View view = activity.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    public final static boolean isValidEmail(CharSequence target) {
+        if (TextUtils.isEmpty(target)) {
+            return false;
+        } else {
+            return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+        }
+    }
+
+    public static boolean isEmptyField(String field) {
+        return field.length()>0;
+    }
+
+    /**
+     *Makes the edittext view focused and capitalized if the last parameter is true
+     *
+     * @param context
+     * @param view
+     * @param isText
+     *
+     */
+    public static void focus(Context context, EditText view, boolean... isText) {
+        view.requestFocus();
+        if (isText != null && isText[0]){
+            view.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_SENTENCES);
+        }
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
+    }
+
+    public static String unixToDate(long timestamp, String s) {
+
+        String date = DateFormat.format(s, timestamp).toString();
+        return date;
+    }
 }
